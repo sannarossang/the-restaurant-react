40d77f18457a0a552f83c92cf5fdbb7601c2ea97
feliciacalming <felicia.calming@gmail.com>
2023-05-31 14:38:38 +0200

    (feat): create BookingSchema

--- fed22s-backend/src/models/Booking.js
+++ fed22s-backend/src/models/Booking.js
@@ -1,1 +1,27 @@
 const mongoose = require("mongoose");
+
+const BookingSchema = new mongoose.Schema({
+  booker: {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+    },
+  },
+});
+
+module.exports = mongoose.model("Booking", BookingSchema);
