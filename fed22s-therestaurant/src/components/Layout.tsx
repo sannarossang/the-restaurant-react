c41f1f48a876b93a1966ff0d10709b1c5c300e92
feliciacalming <felicia.calming@gmail.com>
2023-06-01 18:52:24 +0200

    (feat): add grid wrapper to Layout

--- fed22s-therestaurant/src/components/Layout.tsx
+++ fed22s-therestaurant/src/components/Layout.tsx
@@ -1,13 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { Navbar } from "./Navbar";
+import { ContentWrapper } from "./styled/Layout";
 
 export const Layout = () => {
   return (
     <>
-      <header></header>
-      <main>
-        <Outlet></Outlet>
-      </main>
-      <footer></footer>
+      <ContentWrapper>
+        <header>
+          <Navbar></Navbar>
+        </header>
+        <main>
+          <Outlet></Outlet>
+        </main>
+        <footer></footer>
+      </ContentWrapper>
     </>
   );
 };
---
+++ fed22s-therestaurant/src/components/styled/Layout.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ContentWrapper = styled.div`
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 1fr auto;
+  grid-template-areas:
+    "header"
+    "main"
+    "footer";
+`;
