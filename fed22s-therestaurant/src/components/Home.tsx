9dcb46ce608e300f5d84dd37bc5d46065b3345c3
feliciacalming <felicia.calming@gmail.com>
2023-06-01 19:48:21 +0200

    (feat): checking that axios get request works

--- fed22s-therestaurant/src/components/Home.tsx
+++ fed22s-therestaurant/src/components/Home.tsx
@@ -1,4 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface IBooking {
+  data: [];
+}
+
 export const Home = () => {
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    const getBookings = async () => {
+      const bookings = await axios
+        .get<IBooking>("http://localhost:4000/api/v1/admin")
+        .then((response) => response.data.data);
+
+      console.log(bookings);
+    };
+
+    getBookings();
+    setBookings(bookings);
+  });
+
   return (
     <>
       <h1>Home</h1>
