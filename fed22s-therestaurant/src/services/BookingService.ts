76d9e61963093901219ef9ba61410cf48efbb88a
feliciacalming <felicia.calming@gmail.com>
2023-06-04 17:51:55 +0200

    (feat): add comments

--- fed22s-therestaurant/src/services/BookingService.ts
+++ fed22s-therestaurant/src/services/BookingService.ts
@@ -3,7 +3,13 @@ import { IBooking } from "../models/IBooking";
 import { IBookingsResponse } from "../models/IBookingsResponse";
 
 const BASE_URL = "http://localhost:4000/api/v1";
+/* Hur ska vi göra med admin/booker-routes? Behöver man verkligen ha samma funktioner på båda?
+Isåfall får vi lägga till en variabel för route här också typ. */
 
+/* Provade det här med "get" nedanför pga såg att Sebbe gjort så i en demo, men känns lite onödigt i det här fallet pga
+används bara till en funktion?
+Eller så kan man använda nåt sånt här för alla request och dela upp det i två filer så det blir lite
+mer clean kod. Inte hunnit tänka klart den tanken helt :) */
 const get = async <T>(endpoint: string) => {
   const response = await axios.get<T>(`${BASE_URL}${endpoint}`);
   return response.data;
@@ -20,6 +26,10 @@ export const getBooking = async (id: string): Promise<IBooking> => {
   return await get<IBooking>(`/admin/${id}`);
 };
 
+/* Tänker att "booking" ska komma från ett state, så att man sparar den bokning man klickat på i ett state och 
+sen behöver man bara ändra det man ska ändra och så kommer resten vara samma i statet. Känns DOCK som att det
+här blir samma kod som i backend? Kanske går att reducera koden i backend lite nu eftersom vi alltid kommer
+skicka med ett helt ifyllt boknings-objekt? */
 export const updateBooking = async (
   id: string,
   booking: IBooking
