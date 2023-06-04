5753fba329ff51faf9f900f5e5f4c5d0c1740bd3
feliciacalming <felicia.calming@gmail.com>
2023-06-04 14:49:32 +0200

    (feat): add getBookings and getBooking functions

--- fed22s-therestaurant/src/services/BookingService.ts
+++ fed22s-therestaurant/src/services/BookingService.ts
@@ -1,8 +1,21 @@
 import axios from "axios";
+import { IBooking } from "../models/IBooking";
+import { IBookingsResponse } from "../models/IBookingsResponse";
 
-const BASE_URL = "http://localhost:4000/api/v1/";
+const BASE_URL = "http://localhost:4000/api/v1";
 
 const get = async <T>(endpoint: string) => {
   const response = await axios.get<T>(`${BASE_URL}${endpoint}`);
   return response.data;
+};
+
+export const getBookings = async (): Promise<IBooking[]> => {
+  const response = await axios
+    .get<IBookingsResponse>("http://localhost:4000/api/v1/admin")
+    .then((response) => response.data.data);
+  return response;
+};
+
+export const getBooking = async (): Promise<IBooking> => {
+  return await get<IBooking>("/admin/64784e9f49ae597d8c4f9c7f");
 };
