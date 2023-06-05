85ada02c7888b2a159da0ec14980b541d6d0bd87
feliciacalming <felicia.calming@gmail.com>
2023-06-05 18:04:11 +0200

    (feat): add useForm and styled components with functionality

--- fed22s-therestaurant/src/components/forms/GuestForm.tsx
+++ fed22s-therestaurant/src/components/forms/GuestForm.tsx
@@ -1,3 +1,32 @@
+import { useForm } from "react-hook-form";
+import { GuestInput, StyledGuestForm } from "../styled/Forms/GuestForm";
+
 export const GuestForm = () => {
-  return <></>;
+  const { register, handleSubmit } = useForm();
+  return (
+    <>
+      <StyledGuestForm
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <GuestInput
+          {...register("firstname")}
+          placeholder="Förnamn"
+        ></GuestInput>
+        <GuestInput
+          {...register("lastname")}
+          placeholder="Efternamn"
+        ></GuestInput>
+        <GuestInput
+          {...register("phone")}
+          placeholder="070 XXX XX XX"
+        ></GuestInput>
+        <GuestInput
+          {...register("email")}
+          placeholder="förnamn.efternam@email.com"
+        ></GuestInput>
+      </StyledGuestForm>
+    </>
+  );
 };
