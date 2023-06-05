d3af368ba49a18e8640cbb93a2d7b7cd2ed4f7e7
feliciacalming <felicia.calming@gmail.com>
2023-06-05 09:59:08 +0200

    (feat): add createNewBooking

--- fed22s-therestaurant/src/services/BookingService.ts
+++ fed22s-therestaurant/src/services/BookingService.ts
@@ -3,33 +3,39 @@ import { IBooking } from "../models/IBooking";
 import { IBookingsResponse } from "../models/IBookingsResponse";
 
 const BASE_URL = "http://localhost:4000/api/v1";
-/* Hur ska vi göra med admin/booker-routes? Behöver man verkligen ha samma funktioner på båda?
-Isåfall får vi lägga till en variabel för route här också typ. */
 
-/* Provade det här med "get" nedanför pga såg att Sebbe gjort så i en demo, men känns lite onödigt i det här fallet pga
-används bara till en funktion?
-Eller så kan man använda nåt sånt här för alla request och dela upp det i två filer så det blir lite
-mer clean kod. Inte hunnit tänka klart den tanken helt :) */
-const get = async <T>(endpoint: string) => {
-  const response = await axios.get<T>(`${BASE_URL}${endpoint}`);
-  return response.data;
+export const createNewBooking = async (route: string, booking: IBooking) => {
+  const response = await axios.post(`${BASE_URL}/${route}`, {
+    booker: {
+      firstname: booking.booker.firstname,
+      lastname: booking.booker.lastname,
+      email: booking.booker.email,
+      phone: booking.booker.phone,
+    },
+    guests: booking.guests,
+    seatingTime: booking.seatingTime,
+    seatingDate: booking.seatingDate,
+    message: booking.message,
+  });
+
+  return response;
 };
 
 export const getBookings = async (): Promise<IBooking[]> => {
   const response = await axios
-    .get<IBookingsResponse>("http://localhost:4000/api/v1/admin")
+    .get<IBookingsResponse>(`${BASE_URL}/admin/`)
     .then((response) => response.data.data);
   return response;
 };
 
-export const getBooking = async (id: string): Promise<IBooking> => {
-  return await get<IBooking>(`/admin/${id}`);
+export const getBooking = async (
+  route: string,
+  id: string
+): Promise<IBooking> => {
+  const response = await axios.get<IBooking>(`/${route}/${id}`);
+  return response.data;
 };
 
-/* Tänker att "booking" ska komma från ett state, så att man sparar den bokning man klickat på i ett state och 
-sen behöver man bara ändra det man ska ändra och så kommer resten vara samma i statet. Känns DOCK som att det
-här blir samma kod som i backend? Kanske går att reducera koden i backend lite nu eftersom vi alltid kommer
-skicka med ett helt ifyllt boknings-objekt? */
 export const updateBooking = async (
   id: string,
   booking: IBooking
@@ -50,7 +56,7 @@ export const updateBooking = async (
   return response.data;
 };
 
-export const deleteBooking = async (id: string) => {
-  const response = await axios.delete(`${BASE_URL}/admin/${id}`);
+export const deleteBooking = async (route: string, id: string) => {
+  const response = await axios.delete(`${BASE_URL}/${route}/${id}`);
   return response.status;
 };
