4be0700f50b2986a8228cf729a333f6c9033ff05
feliciacalming <felicia.calming@gmail.com>
2023-06-05 16:55:35 +0200

    (feat): add navigation Link to button

--- fed22s-therestaurant/src/components/Home.tsx
+++ fed22s-therestaurant/src/components/Home.tsx
@@ -1,11 +1,19 @@
-import { BookButton, HomeWrapper, RestaurantTitle } from "./styled/Home/Home";
+import { Link } from "react-router-dom";
+import {
+  BookButton,
+  HomeWrapper,
+  RestaurantTitle,
+  StyledLink,
+} from "./styled/Home/Home";
 
 export const Home = () => {
   return (
     <>
       <HomeWrapper>
         <RestaurantTitle>BOOKER</RestaurantTitle>
-        <BookButton>Boka bord</BookButton>
+        <Link to="/booking">
+          <BookButton>Boka bord</BookButton>
+        </Link>
       </HomeWrapper>
     </>
   );
