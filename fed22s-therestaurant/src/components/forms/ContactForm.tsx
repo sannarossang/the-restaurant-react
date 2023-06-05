7bbccb2ba218bb67b73018ec821cead4a7a303e1
Sanna Rossäng <113436657+sannarossang@users.noreply.github.com>
2023-06-05 20:56:20 +0200

    (feature)): adding basic structure for form

--- fed22s-therestaurant/src/components/forms/ContactForm.tsx
+++ fed22s-therestaurant/src/components/forms/ContactForm.tsx
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 export const ContactForm = () => {
-  return <></>;
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setSubmitted(true);
+  };
+
+  if (submitted) {
+    return (
+      <>
+        <h2>Thanks! We'll contact you asap!</h2>
+      </>
+    );
+  }
+  return (
+    <>
+      <div>
+        <h1>Contact us</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <input type="text" placeholder="Firstname" name="firstname" required />
+          </div>
+          <div>
+            <input type="text" placeholder="Lastname" name="lastname" required />
+          </div>
+          <div>
+            <input type="text" placeholder="Email" name="email" required />
+          </div>
+          <div>
+            <input type="text" placeholder="Message" name="message" required />
+          </div>
+          <div>
+            <button type="submit">Contact us</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
