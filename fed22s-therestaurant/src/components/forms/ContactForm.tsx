e56256b9da6be4cd428edce76c440bcb6e816748
Sanna Rossäng <113436657+sannarossang@users.noreply.github.com>
2023-06-06 07:42:54 +0200

    (feature): adding useForm-hook and  functionality

--- fed22s-therestaurant/src/components/forms/ContactForm.tsx
+++ fed22s-therestaurant/src/components/forms/ContactForm.tsx
@@ -2,35 +2,82 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export const ContactForm = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const handleSubmit = () => {
-    setSubmitted(true);
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstname: "Sebastian",
+      lastname: "Tegel",
+      email: "sebbe@tegel.com",
+      message: "Älskar er restaurang, men...",
+    },
+  });
 
-  if (submitted) {
-    return (
-      <>
-        <h2>Thanks! We'll contact you asap!</h2>
-      </>
-    );
-  }
+  //   const [submitted, setSubmitted] = useState(false);
+  //   const handleSubmit = () => {
+  //     setSubmitted(true);
+  //   };
+
+  //   if (submitted) {
+  //     return (
+  //       <>
+  //         <h2>Thanks! We'll contact you asap!</h2>
+  //       </>
+  //     );
+  //   }
+
+  console.log(errors);
+
   return (
     <>
       <div>
         <h1>Contact us</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <input type="text" placeholder="Firstname" name="firstname" required />
-          </div>
-          <div>
-            <input type="text" placeholder="Lastname" name="lastname" required />
-          </div>
-          <div>
-            <input type="text" placeholder="Email" name="email" required />
-          </div>
-          <div>
-            <input type="text" placeholder="Message" name="message" required />
-          </div>
+        <form
+          onSubmit={handleSubmit(data => {
+            console.log(data);
+          })}
+        >
+          <input
+            {...register("firstname", {
+              required: "Du måste skriva ditt förnamn",
+              minLength: { value: 2, message: "Du måste skriva minst två bokstäver" },
+            })}
+            type="text"
+          />
+          <p>{errors.firstname?.message}</p>
+
+          <input
+            {...register("lastname", {
+              required: "Du måste skriva ditt efternamn",
+              minLength: { value: 2, message: "Du måste skriva minst två bokstäver" },
+            })}
+            type="text"
+            name="lastname"
+          />
+          <p>{errors.lastname?.message}</p>
+
+          <input
+            {...register("email", {
+              required: "Du måste skriva din mejl",
+              minLength: { value: 2, message: "Du måste skriva minst två bokstäver" },
+            })}
+            type="text"
+            name="email"
+          />
+          <p>{errors.email?.message}</p>
+
+          <input
+            {...register("message", {
+              required: "Du måste skriva ditt meddelande",
+              minLength: { value: 2, message: "Du måste skriva minst två bokstäver" },
+            })}
+            type="text"
+            name="message"
+          />
+          <p>{errors.message?.message}</p>
+
           <div>
             <button type="submit">Contact us</button>
           </div>
