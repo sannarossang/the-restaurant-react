2e0d0254bb640d770630bed4ccb34878afce9b68
Sanna Rossäng <113436657+sannarossang@users.noreply.github.com>
2023-06-06 11:09:10 +0200

    (refactor): added schema for zod

--- fed22s-therestaurant/src/components/forms/ContactForm.tsx
+++ fed22s-therestaurant/src/components/forms/ContactForm.tsx
@@ -1,15 +1,25 @@
 import { useEffect, useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm, useController } from "react-hook-form";
 import { IContactFormInput } from "../../models/IContactFormInput";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { string, z } from "zod";
+
+const schema = z.object({
+  firstname: string().min(2, { message: "Du måste skriva ett efternamn som är minst två tecken långt" }),
+  lastname: string().min(2, { message: "Du måste skriva ett efternamn som är minst två tecken långt" }),
+  email: string().email({ message: "Du måste skriva en korrekt emailadress" }),
+  message: string().optional(),
+});
 
 export const ContactForm = () => {
   const {
     register,
     handleSubmit,
     formState,
-    formState: { errors, dirtyFields, touchedFields, isValid },
+    formState: { dirtyFields, touchedFields, isValid },
   } = useForm<IContactFormInput>({
     mode: "onChange",
+    resolver: zodResolver(schema),
     defaultValues: {
       firstname: "",
       lastname: "",
@@ -18,81 +28,60 @@ export const ContactForm = () => {
     },
   });
 
+  const { errors } = formState;
   const onSubmit: SubmitHandler<IContactFormInput> = data => console.log(data);
-
-  //   const [submitted, setSubmitted] = useState(false);
-  //   const handleSubmitNew = () => {
-  //     setSubmitted(true);
-  //   };
-
-  //   if (submitted) {
-  //     return (
-  //       <>
-  //         <h2>Thanks! We'll contact you asap!</h2>
-  //       </>
-  //     );
-  //   }
-
-  console.log(errors);
-  console.log("dirtyFields", dirtyFields);
-  console.log("touchedFields", touchedFields);
-  console.log("isValid", isValid);
-
-  useEffect(() => {
-    console.log("useEffect", formState.errors);
-  }, [formState]);
 
   return (
     <>
       <div>
         <h1>Contact us</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            {...register("firstname", {
-              required: "Du måste skriva ditt förnamn",
-              pattern: /^[a-zA-Z]+$/,
-              minLength: { value: 2, message: "Du måste skriva minst två bokstäver" },
-            })}
-            type="text"
-            placeholder="firstname"
-          />
-          <p>{errors.firstname?.message}</p>
+          <div>
+            <input
+              {...register("firstname", {
+                pattern: /^[a-zA-Z]+$/, //funkar inte
+              })}
+              type="text"
+              placeholder="firstname"
+            />
+            <p>{errors.firstname?.message}</p>
+          </div>
+          <div>
+            <input
+              {...register("lastname", {
+                pattern: /^[a-zA-Z]+$/, //funkar inte
+              })}
+              type="text"
+              name="lastname"
+              placeholder="lastname"
+            />
+            <p>{errors.lastname?.message}</p>
+          </div>
 
-          <input
-            {...register("lastname", {
-              required: "Du måste skriva ditt efternamn",
-              pattern: /^[a-zA-Z]+$/,
-              minLength: { value: 2, message: "Du måste skriva minst två bokstäver" },
-            })}
-            type="text"
-            name="lastname"
-            placeholder="lastname"
-          />
-          <p>{errors.lastname?.message}</p>
+          <div>
+            <input
+              {...register("email", {
+                pattern: /^\S+@\S+$/i,
+              })}
+              type="text"
+              name="email"
+              placeholder="email"
+            />
+            <p>{errors.email?.message}</p>
+          </div>
 
-          <input
-            {...register("email", {
-              required: "Du måste skriva din mejl",
-              pattern: /^\S+@\S+$/i,
-              minLength: { value: 2, message: "Du måste skriva minst två bokstäver" },
-            })}
-            type="text"
-            name="email"
-            placeholder="email"
-          />
-          <p>{errors.email?.message}</p>
-
-          <input
-            {...register("message", {
-              required: "Du måste skriva ditt meddelande",
-              maxLength: 20,
-              minLength: { value: 2, message: "Du måste skriva minst två bokstäver" },
-            })}
-            type="text"
-            name="message"
-            placeholder="message"
-          />
-          <p>{errors.message?.message}</p>
+          <div>
+            <input
+              {...register("message", {
+                maxLength: 20,
+                minLength: { value: 2, message: "Du måste skriva minst två bokstäver" },
+              })}
+              type="text"
+              name="message"
+              placeholder="message"
+            />
+            <p>{errors.message?.message}</p>
+          </div>
 
           <div>
             <button type="submit">Contact us</button>
