c96c6f3cf0f2eef4fa631d1943488958377d9250
feliciacalming <felicia.calming@gmail.com>
2023-06-06 10:34:28 +0200

    (feat): update styling for GuestForm

--- fed22s-therestaurant/src/components/styled/Forms/GuestForm.ts
+++ fed22s-therestaurant/src/components/styled/Forms/GuestForm.ts
@@ -1,8 +1,21 @@
 import styled from "styled-components";
 
+export const GuestFormWrapper = styled.div``;
+
 export const StyledGuestForm = styled.form`
   display: flex;
+  width: 80vw;
   flex-direction: column;
+  align-items: center;
+
+  button {
+    width: 50px;
+  } /* ska vi göra en återanvändningsbar knapp för både detta och contact form? 
+  eller vill vi ha såpass olika styling på dom? 
+  */
 `;
 
-export const GuestInput = styled.input``;
+export const GuestInput = styled.input`
+  width: 100%;
+  height: 10vh;
+`;
