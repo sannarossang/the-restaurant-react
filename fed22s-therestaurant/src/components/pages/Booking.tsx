aa097bf6751a2f2c8b4bf66c88325b655db0add4
feliciacalming <felicia.calming@gmail.com>
2023-06-07 15:38:03 +0200

    (feat): add components to Booking component

--- fed22s-therestaurant/src/components/pages/Booking.tsx
+++ fed22s-therestaurant/src/components/pages/Booking.tsx
@@ -1,7 +1,16 @@
+import { AmountOfGuestsInput } from "../AmountOfGuestsInput";
+import { AvailableTimes } from "../AvailableTimes";
+import { CalendarView } from "../Calendar";
+import { GuestForm } from "../forms/GuestForm";
+
 export const Booking = () => {
   return (
     <>
       <h1>Booking</h1>
+      <AmountOfGuestsInput />
+      <CalendarView />
+      <AvailableTimes />
+      <GuestForm />
     </>
   );
 };
