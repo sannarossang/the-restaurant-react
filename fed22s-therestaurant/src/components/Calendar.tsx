c5a43c4f3812a8ea2965d4772b16e3dc7a57dfb2
feliciacalming <felicia.calming@gmail.com>
2023-06-07 10:13:20 +0200

    (fix): fix bug with datatype by adding type Value

--- fed22s-therestaurant/src/components/Calendar.tsx
+++ fed22s-therestaurant/src/components/Calendar.tsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 export const CalendarView = () => {
+  const [date, setDate] = useState<Value>(new Date());
+
+  useEffect(() => {
+    console.log(date);
+  }, [date]);
+
   return (
     <>
-      <Calendar></Calendar>
+      <Calendar onChange={setDate} value={date}></Calendar>
     </>
   );
 };
