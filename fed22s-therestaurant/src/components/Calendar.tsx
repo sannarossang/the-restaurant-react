df76c242f098e99ae2c98fe529ecea7eba93f1f9
feliciacalming <felicia.calming@gmail.com>
2023-06-08 11:10:22 +0200

    (feat): import react calendar css

--- fed22s-therestaurant/src/components/Calendar.tsx
+++ fed22s-therestaurant/src/components/Calendar.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
