4e803d55125035bbf9328f60758e426d4ff5326d
feliciacalming <felicia.calming@gmail.com>
2023-06-09 15:16:36 +0200

    (feat): add defaultBookingValues

---
+++ fed22s-therestaurant/src/models/defaultBookingValues.ts
@@ -0,0 +1,14 @@
+export const defaultBookingValues = {
+  booker: {
+    firstname: "",
+    lastname: "",
+    email: "",
+    phone: "",
+  },
+
+  guests: 0,
+  message: "",
+  seatingTime: "",
+  seatingDate: "",
+  _id: "",
+};
