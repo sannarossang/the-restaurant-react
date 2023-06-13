97b8b2190bdcec4c26fe3be7ec64e179cc68a0cd
Sanna Rossäng <113436657+sannarossang@users.noreply.github.com>
2023-06-13 13:27:16 +0200

    (fix): push same code

--- fed22s-therestaurant/src/components/pages/Booking.tsx
+++ fed22s-therestaurant/src/components/pages/Booking.tsx
@@ -1,18 +1,12 @@
-import { useReducer } from "react";
-import {
-  CurrentBookingContext,
-  CurrentBookingDispatchContext,
-} from "../../contexts/CurrentBookingContext";
+import { useReducer, useState } from "react";
+import { CurrentBookingContext, CurrentBookingDispatchContext } from "../../contexts/CurrentBookingContext";
 import { CurrentBookingReducer } from "../../reducers/CurrentBookingReducer";
 import { BookingForm } from "../forms/BookingForm";
 import { defaultBookingValues } from "../../models/defaultBookingValues";
 import { BookingSummary } from "../BookingSummary";
 
 export const Booking = () => {
-  const [currentBooking, dispatch] = useReducer(
-    CurrentBookingReducer,
-    defaultBookingValues
-  );
+  const [currentBooking, dispatch] = useReducer(CurrentBookingReducer, defaultBookingValues);
   return (
     <>
       <CurrentBookingContext.Provider value={currentBooking}>
