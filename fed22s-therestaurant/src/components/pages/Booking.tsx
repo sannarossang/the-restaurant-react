a5c00fed14974535318a1454ca1cd44e1d488107
feliciacalming <felicia.calming@gmail.com>
2023-06-14 20:07:28 +0200

    (feat): add styled components to Booking

--- fed22s-therestaurant/src/components/pages/Booking.tsx
+++ fed22s-therestaurant/src/components/pages/Booking.tsx
@@ -1,20 +1,37 @@
 import { useReducer, useState } from "react";
-import { CurrentBookingContext, CurrentBookingDispatchContext } from "../../contexts/CurrentBookingContext";
+import {
+  CurrentBookingContext,
+  CurrentBookingDispatchContext,
+} from "../../contexts/CurrentBookingContext";
 import { CurrentBookingReducer } from "../../reducers/CurrentBookingReducer";
 import { BookingForm } from "../forms/BookingForm";
 import { defaultBookingValues } from "../../models/defaultBookingValues";
 import { BookingSummary } from "../BookingSummary";
 import { BookingConfirmation } from "../BookingConfirmation";
+import { BookingModal, BookingWrapper } from "../styled/Booking/Booking";
 
 export const Booking = () => {
-  const [currentBooking, dispatch] = useReducer(CurrentBookingReducer, defaultBookingValues);
+  const [currentBooking, dispatch] = useReducer(
+    CurrentBookingReducer,
+    defaultBookingValues
+  );
   return (
     <>
       <CurrentBookingContext.Provider value={currentBooking}>
         <CurrentBookingDispatchContext.Provider value={dispatch}>
-          {!currentBooking.booker.email ? <BookingForm /> : <></>}
-          {currentBooking.booker.email && !currentBooking._id ? <BookingSummary /> : <></>}
-          {currentBooking._id ? <BookingConfirmation /> : <></>}
+          <BookingWrapper>
+            <BookingModal>
+              {/* <BookingModal> */}
+              {!currentBooking.booker.email ? <BookingForm /> : <></>}
+              {currentBooking.booker.email && !currentBooking._id ? (
+                <BookingSummary />
+              ) : (
+                <></>
+              )}
+              {currentBooking._id ? <BookingConfirmation /> : <></>}
+              {/* </BookingModal> */}
+            </BookingModal>
+          </BookingWrapper>
         </CurrentBookingDispatchContext.Provider>
       </CurrentBookingContext.Provider>
     </>
