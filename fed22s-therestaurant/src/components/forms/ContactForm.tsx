6a31804e849939691cdd2bf737d56f5f2f7ed8ff
Sanna Rossäng <113436657+sannarossang@users.noreply.github.com>
2023-06-15 19:49:58 +0200

    (style): styling contact form

--- fed22s-therestaurant/src/components/forms/ContactForm.tsx
+++ fed22s-therestaurant/src/components/forms/ContactForm.tsx
@@ -3,37 +3,39 @@ import { IContactFormInput } from "../../models/IContactFormInput";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { string, z } from "zod";
 import {
+  ContactButton,
   ContactFormWrapper,
   ContactTitle,
+  InputContactForm,
+  InputContactWrapper,
+  ValidationErrorContactForm,
 } from "../styled/Contact/ContactForm";
-import { BookButton } from "../styled/Home/Home";
 
 const schema = z.object({
   firstname: string().min(2, {
-    message: "Du måste skriva ett efternamn som är minst två tecken långt",
+    message: "Du måste skriva ett förnamn.",
   }),
   lastname: string().min(2, {
-    message: "Du måste skriva ett efternamn som är minst två tecken långt",
+    message: "Du måste skriva ett efternamn.",
   }),
   email: string().email({ message: "Du måste skriva en korrekt emailadress" }),
   message: string().optional(),
 });
 
 export const ContactForm = () => {
-  const { register, handleSubmit, reset, formState } =
-    useForm<IContactFormInput>({
-      mode: "onChange",
-      resolver: zodResolver(schema),
-      defaultValues: {
-        firstname: "",
-        lastname: "",
-        email: "",
-        message: "",
-      },
-    });
+  const { register, handleSubmit, reset, formState } = useForm<IContactFormInput>({
+    mode: "onChange",
+    resolver: zodResolver(schema),
+    defaultValues: {
+      firstname: "",
+      lastname: "",
+      email: "",
+      message: "",
+    },
+  });
 
   const { errors } = formState;
-  const onSubmit: SubmitHandler<IContactFormInput> = (data) => {
+  const onSubmit: SubmitHandler<IContactFormInput> = data => {
     console.log(data);
     reset();
   };
@@ -41,59 +43,41 @@ export const ContactForm = () => {
   return (
     <>
       <ContactFormWrapper onSubmit={handleSubmit(onSubmit)}>
-        <ContactTitle>Contact us</ContactTitle>
-        <div>
-          <input
-            {...register("firstname", {
-              pattern: /^[a-zA-Z]+$/, //funkar inte
-            })}
-            type="text"
-            placeholder="firstname"
-          />
-          <p>{errors.firstname?.message}</p>
-        </div>
-        <div>
-          <input
-            {...register("lastname", {
-              pattern: /^[a-zA-Z]+$/, //funkar inte
-            })}
-            type="text"
-            name="lastname"
-            placeholder="lastname"
-          />
-          <p>{errors.lastname?.message}</p>
-        </div>
+        <ContactTitle>Kontakta oss</ContactTitle>
+        <InputContactWrapper>
+          <div>
+            <InputContactForm {...register("firstname")} type="text" placeholder="FÖRNAMN" />
+            <ValidationErrorContactForm>{errors.firstname?.message}</ValidationErrorContactForm>
+          </div>
+          <div>
+            <InputContactForm {...register("lastname")} type="text" name="lastname" placeholder="EFTERNAMN" />
+            <ValidationErrorContactForm>{errors.lastname?.message}</ValidationErrorContactForm>
+          </div>
+
+          <div>
+            <InputContactForm {...register("email")} type="text" name="email" placeholder="EPOSTADRESS" />
+            <ValidationErrorContactForm>{errors.email?.message}</ValidationErrorContactForm>
+          </div>
+
+          <div>
+            <InputContactForm
+              {...register("message", {
+                maxLength: 20,
+                minLength: {
+                  value: 2,
+                  message: "Du måste skriva minst två bokstäver",
+                },
+              })}
+              type="text"
+              name="message"
+              placeholder="MEDDELANDE"
+            />
+            <ValidationErrorContactForm>{errors.message?.message}</ValidationErrorContactForm>
+          </div>
+        </InputContactWrapper>
 
         <div>
-          <input
-            {...register("email", {
-              pattern: /^\S+@\S+$/i,
-            })}
-            type="text"
-            name="email"
-            placeholder="email"
-          />
-          <p>{errors.email?.message}</p>
-        </div>
-
-        <div>
-          <input
-            {...register("message", {
-              maxLength: 20,
-              minLength: {
-                value: 2,
-                message: "Du måste skriva minst två bokstäver",
-              },
-            })}
-            type="text"
-            name="message"
-            placeholder="message"
-          />
-          <p>{errors.message?.message}</p>
-        </div>
-
-        <div>
-          <BookButton type="submit">Spara skiten</BookButton>
+          <ContactButton type="submit">SKICKA</ContactButton>
         </div>
       </ContactFormWrapper>
     </>
