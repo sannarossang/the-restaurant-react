3cffd7722dd5592c193df9be5001726361e129a5
feliciacalming <felicia.calming@gmail.com>
2023-06-15 21:30:37 +0200

    (style): update navbar styling

--- fed22s-therestaurant/src/components/styled/Navbar.ts
+++ fed22s-therestaurant/src/components/styled/Navbar.ts
@@ -1,25 +1,44 @@
 import styled from "styled-components";
+import { Color } from "../../styles/colors";
+import { DeviceQuery } from "../../styles/breakpoints";
 
 export const NavWrapper = styled.div`
   width: 100vw;
-  color: white;
-  background-color: black;
+  color: ${Color.PRIMARY_DARK};
+  background-color: rgba(146, 146, 146, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 8vh;
   margin: 0;
   padding: 0;
 `;
 
 export const Nav = styled.ul`
-  display: inline-flex;
-  gap: 20px;
+  width: 80%;
+  margin-top: 10px;
+  padding: 0;
+  display: flex;
+  justify-content: space-between;
+  font-weight: bold;
+  font-size: 0.8rem;
+  gap: 5%;
+  color: ${Color.PRIMARY_LIGHT};
 
   a {
     text-decoration: none;
-    color: white;
+    color: ${Color.PRIMARY_LIGHT};
 
     &:hover {
-      color: pink;
+      text-shadow: 1px 1px ${Color.TRANSPARENT_PINK};
     }
+  }
+
+  @media ${DeviceQuery.DESKTOP} {
+    margin-top: 1%;
+    justify-content: space-between;
+    color: ${Color.PRIMARY_LIGHT};
+    gap: 20px;
   }
 `;
 
