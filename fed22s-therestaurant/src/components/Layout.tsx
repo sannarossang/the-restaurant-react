6d47f75ed93ab1a2b4be1c247427d63ed7274575
feliciacalming <felicia.calming@gmail.com>
2023-06-15 21:30:28 +0200

    (feat): add location check

--- fed22s-therestaurant/src/components/Layout.tsx
+++ fed22s-therestaurant/src/components/Layout.tsx
@@ -1,11 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Navbar } from "./Navbar";
 import { ContentWrapper } from "./styled/Layout";
+import { useEffect, useState } from "react";
 
 export const Layout = () => {
+  const [filter, setFilter] = useState("");
+  const location = useLocation();
+
+  useEffect(() => {
+    const checkLocation = () => {
+      if (location.pathname === "/") setFilter("grayscale(100%)");
+      if (location.pathname === "/booking")
+        setFilter(`grayscale(100%) brightness(20%)`);
+    };
+
+    checkLocation();
+  }, [location]);
+
+  console.log(filter);
+
   return (
     <>
-      <ContentWrapper>
+      <ContentWrapper
+        className={location.pathname === "/" || "/booking" ? "home" : ""}
+        filter={filter}
+      >
         <header>
           <Navbar></Navbar>
         </header>
