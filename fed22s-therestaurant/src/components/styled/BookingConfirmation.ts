a49eb68b0276393aa904c47a44125378059c4b74
Sanna Rossäng <113436657+sannarossang@users.noreply.github.com>
2023-06-15 10:58:42 +0200

    (style): styled confirmation

---
+++ fed22s-therestaurant/src/components/styled/BookingConfirmation.ts
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import { DeviceQuery } from "../../styles/breakpoints";
+
+interface ConfirmationProps {
+  weight?: string;
+  size?: string;
+  padding?: string;
+}
+
+export const ConfirmationWrapper = styled.div`
+  display: flex;
+  text-align: left;
+  flex-direction: column;
+  padding: 30px;
+  width: 70%;
+
+  @media ${DeviceQuery.DESKTOP} {
+  }
+`;
+
+export const ConfirmationTextBox = styled.div`
+  margin: 0px;
+  padding-bottom: 30px;
+
+  @media ${DeviceQuery.DESKTOP} {
+  }
+`;
+
+export const ConfirmationDetails = styled.div`
+  background-color: #fcf3e8;
+  width: 90%;
+
+  @media ${DeviceQuery.DESKTOP} {
+    width: 70%;
+  }
+`;
+
+export const ConfirmationReference = styled.div`
+  margin: 20px;
+  border-bottom: 1px solid;
+  @media ${DeviceQuery.DESKTOP} {
+  }
+`;
+
+export const ConfirmationGuestBox = styled.div`
+  margin: 20px;
+  border-bottom: 1px solid;
+  @media ${DeviceQuery.DESKTOP} {
+  }
+`;
+
+export const ConfirmationText = styled.p<ConfirmationProps>`
+  font-size: ${(props: ConfirmationProps) => props.size || "12px"};
+  font-weight: ${(props: ConfirmationProps) => props.weight};
+  @media ${DeviceQuery.DESKTOP} {
+  }
+`;
